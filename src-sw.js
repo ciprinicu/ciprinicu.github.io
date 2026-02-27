9c2666021f43645e07164734075133a04085a2d5
ciprinicu <42282352+ciprinicu@users.noreply.github.com>
2026-02-27 05:12:21 +0000

    Deploying to gh-pages from @ ciprinicu/ciprinicu.github.io@9d5335e8ec0ee5b7a34cc026e76f54e94a0f0d11 🚀

--- src-sw.js
+++ src-sw.js
@@ -1,6 +1,6 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js');
 
-const CACHE_VERSION = 'v0.3.5';
+const CACHE_VERSION = 'v0.3.6';
 
 // Verificăm dacă s-a încărcat Workbox
 if (workbox) {
